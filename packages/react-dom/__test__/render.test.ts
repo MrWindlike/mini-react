2c62577e54b31f9f7ded359fc8eb1abee98a1b34
MrWindlike <MrWindlike@gmail.com>
2022-01-29 21:29:23 +0800

    :sparkles: feat(react-dom): add `render` function

---
+++ packages/react-dom/__test__/render.test.ts
@@ -0,0 +1,26 @@
+import { createElement } from '@local/react'
+import { render } from '../src/render'
+
+describe('Render ReactElement', () => {
+  it('Render a element without children', () => {
+    const element = createElement('div', {}, 'Hello World')
+    const container = document.createElement('div')
+
+    render(element, container)
+
+    expect(container.innerHTML).toBe('<div>Hello World</div>')
+  })
+
+  it('Render a element with children', () => {
+    const element = createElement(
+      'div',
+      {},
+      createElement('a', { href: 'https://www.google.com' }, 'Hello World')
+    )
+    const container = document.createElement('div')
+
+    render(element, container)
+
+    expect(container.innerHTML).toBe('<div><a href="https://www.google.com">Hello World</a></div>')
+  })
+})
